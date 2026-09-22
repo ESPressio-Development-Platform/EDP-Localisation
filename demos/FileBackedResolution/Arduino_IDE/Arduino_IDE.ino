91d4b0464bcfc575755092c27f369130358f563b
Simon Stuart <lakraven@gmail.com>
2026-09-22 17:05:10 +0200

    #6 Narrow demo Portable dependency to ByteOperations

--- demos/FileBackedResolution/Arduino_IDE/Arduino_IDE.ino
+++ demos/FileBackedResolution/Arduino_IDE/Arduino_IDE.ino
@@ -5,7 +5,7 @@
 
 #include <ESPressio_Localisation_Persistence.hpp>
 #include <ESPressio_Persistence_Arduino.hpp>
-#include <ESPressio_Platform_Portable.hpp>
+#include <memory/ByteOperationsProvider.hpp>
 
 #include "DemoGenerated.hpp"
 
